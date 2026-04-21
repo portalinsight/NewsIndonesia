ea082e4ceef655c478cc747f5dc860abda9ecb53
portalinsight <portalinsight335@gmail.com>
2026-04-21 19:30:24 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'insight-news-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/breaking.html',
+  '/trending.html',
+  '/hot.html',
+  '/politik.html',
+  '/daerah.html',
+  '/ekonomi.html',
+  '/detail.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
